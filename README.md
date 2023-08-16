043209fda705a5a192c63d9d7de51435fa45e372
trducng <trungduc1992@gmail.com>
2023-08-16 14:56:48 +0700

    Initiate repository

---
+++ README.md
@@ -0,0 +1,3 @@
+Modules:
+- documents: define document
+- loaders
---
+++ knowledgehub/loaders/base.py
@@ -0,0 +1,13 @@
+class DocumentLoader:
+    """Document loader"""
+    pass
+
+
+class TextManipulator:
+    """Text manipulation"""
+    pass
+
+
+class DocumentManipulator:
+    """Document manipulation"""
+    pass
