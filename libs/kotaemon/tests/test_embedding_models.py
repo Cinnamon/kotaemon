921c18e1a3c3754c28f833c41b0c760b60ff6aa5
trducng <trungduc1992@gmail.com>
2024-04-07 04:34:20 +0700

    Add test for AzureOpenAIEmbeddings and OpenAIEmbeddings

--- libs/kotaemon/tests/test_embedding_models.py
+++ libs/kotaemon/tests/test_embedding_models.py
@@ -2,25 +2,29 @@ import json
 from pathlib import Path
 from unittest.mock import patch
 
+from openai.types.create_embedding_response import CreateEmbeddingResponse
+
 from kotaemon.base import Document
 from kotaemon.embeddings import (
+    AzureOpenAIEmbeddings,
     LCAzureOpenAIEmbeddings,
     LCCohereEmbdeddings,
     LCHuggingFaceEmbeddings,
+    OpenAIEmbeddings,
 )
 
 with open(Path(__file__).parent / "resources" / "embedding_openai_batch.json") as f:
-    openai_embedding_batch = json.load(f)
+    openai_embedding_batch = CreateEmbeddingResponse.model_validate(json.load(f))
 
 with open(Path(__file__).parent / "resources" / "embedding_openai.json") as f:
-    openai_embedding = json.load(f)
+    openai_embedding = CreateEmbeddingResponse.model_validate(json.load(f))
 
 
 @patch(
     "openai.resources.embeddings.Embeddings.create",
     side_effect=lambda *args, **kwargs: openai_embedding,
 )
-def test_azureopenai_embeddings_raw(openai_embedding_call):
+def test_lcazureopenai_embeddings_raw(openai_embedding_call):
     model = LCAzureOpenAIEmbeddings(
         model="text-embedding-ada-002",
         deployment="embedding-deployment",
@@ -39,12 +43,84 @@ def test_azureopenai_embeddings_raw(openai_embedding_call):
     "openai.resources.embeddings.Embeddings.create",
     side_effect=lambda *args, **kwargs: openai_embedding_batch,
 )
-def test_azureopenai_embeddings_batch_raw(openai_embedding_call):
+def test_lcazureopenai_embeddings_batch_raw(openai_embedding_call):
     model = LCAzureOpenAIEmbeddings(
         model="text-embedding-ada-002",
         deployment="embedding-deployment",
         azure_endpoint="https://test.openai.azure.com/",
         openai_api_key="some-key",
+    )
+    output = model(["Hello world", "Goodbye world"])
+    assert isinstance(output, list)
+    assert isinstance(output[0], Document)
+    assert isinstance(output[0].embedding, list)
+    assert isinstance(output[0].embedding[0], float)
+    openai_embedding_call.assert_called()
+
+
+@patch(
+    "openai.resources.embeddings.Embeddings.create",
+    side_effect=lambda *args, **kwargs: openai_embedding,
+)
+def test_azureopenai_embeddings_raw(openai_embedding_call):
+    model = AzureOpenAIEmbeddings(
+        azure_endpoint="https://test.openai.azure.com/",
+        api_key="some-key",
+        api_version="version",
+        azure_deployment="text-embedding-ada-002",
+    )
+    output = model("Hello world")
+    assert isinstance(output, list)
+    assert isinstance(output[0], Document)
+    assert isinstance(output[0].embedding, list)
+    assert isinstance(output[0].embedding[0], float)
+    openai_embedding_call.assert_called()
+
+
+@patch(
+    "openai.resources.embeddings.Embeddings.create",
+    side_effect=lambda *args, **kwargs: openai_embedding_batch,
+)
+def test_azureopenai_embeddings_batch_raw(openai_embedding_call):
+    model = AzureOpenAIEmbeddings(
+        azure_endpoint="https://test.openai.azure.com/",
+        api_key="some-key",
+        api_version="version",
+        azure_deployment="text-embedding-ada-002",
+    )
+    output = model(["Hello world", "Goodbye world"])
+    assert isinstance(output, list)
+    assert isinstance(output[0], Document)
+    assert isinstance(output[0].embedding, list)
+    assert isinstance(output[0].embedding[0], float)
+    openai_embedding_call.assert_called()
+
+
+@patch(
+    "openai.resources.embeddings.Embeddings.create",
+    side_effect=lambda *args, **kwargs: openai_embedding,
+)
+def test_openai_embeddings_raw(openai_embedding_call):
+    model = OpenAIEmbeddings(
+        api_key="some-key",
+        model="text-embedding-ada-002",
+    )
+    output = model("Hello world")
+    assert isinstance(output, list)
+    assert isinstance(output[0], Document)
+    assert isinstance(output[0].embedding, list)
+    assert isinstance(output[0].embedding[0], float)
+    openai_embedding_call.assert_called()
+
+
+@patch(
+    "openai.resources.embeddings.Embeddings.create",
+    side_effect=lambda *args, **kwargs: openai_embedding_batch,
+)
+def test_openai_embeddings_batch_raw(openai_embedding_call):
+    model = OpenAIEmbeddings(
+        api_key="some-key",
+        model="text-embedding-ada-002",
     )
     output = model(["Hello world", "Goodbye world"])
     assert isinstance(output, list)
@@ -62,7 +138,7 @@ def test_azureopenai_embeddings_batch_raw(openai_embedding_call):
     "langchain.embeddings.huggingface.HuggingFaceBgeEmbeddings.embed_documents",
     side_effect=lambda *args, **kwargs: [[1.0, 2.1, 3.2]],
 )
-def test_huggingface_embeddings(
+def test_lchuggingface_embeddings(
     langchain_huggingface_embedding_call, sentence_transformers_init
 ):
     model = LCHuggingFaceEmbeddings(
@@ -84,7 +160,7 @@ def test_huggingface_embeddings(
     "langchain.embeddings.cohere.CohereEmbeddings.embed_documents",
     side_effect=lambda *args, **kwargs: [[1.0, 2.1, 3.2]],
 )
-def test_cohere_embeddings(langchain_cohere_embedding_call):
+def test_lccohere_embeddings(langchain_cohere_embedding_call):
     model = LCCohereEmbdeddings(
         model="embed-english-light-v2.0", cohere_api_key="my-api-key"
     )
