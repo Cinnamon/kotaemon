5286ff48bcf9bc4c39a5df77d3b487d585ca1973
ian_Cin <ian@cinnamon.is>
2024-04-14 09:34:14 +0700

    Fix info panel overflow (#33)
    * update chatbot placeholder
    
    * fix chat info panel overflow bug
    
    * set azure_endpoint to required in AzureChatOpenAI
    
    * update screenshots

--- libs/ktem/ktem/pages/chat/chat_panel.py
+++ libs/ktem/ktem/pages/chat/chat_panel.py
@@ -10,7 +10,10 @@ class ChatPanel(BasePage):
     def on_building_ui(self):
         self.chatbot = gr.Chatbot(
             label="Kotaemon",
-            placeholder="This is the beginning of a new conversation.",
+            placeholder=(
+                "This is the beginning of a new conversation.\nMake sure to have added"
+                " a LLM by following the instructions in the Help tab."
+            ),
             show_label=False,
             elem_id="main-chat-bot",
             show_copy_button=True,
