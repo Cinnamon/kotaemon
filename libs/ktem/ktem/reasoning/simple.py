417b6d1c484f540c3d781ed30b605dd08d281243
doviettung96 <tunga9td@gmail.com>
2024-06-03 10:02:08 +0700

    Put the functions to separate modules

--- libs/ktem/ktem/reasoning/simple.py
+++ libs/ktem/ktem/reasoning/simple.py
@@ -10,17 +10,13 @@ from pathlib import Path
 from typing import Generator
 
 import numpy as np
-import langchain_core.messages
 import tiktoken
 from ktem.llms.manager import llms
-from ktem.utils.render import Render
-from langchain.prompts import (
-    ChatPromptTemplate,
-    FewShotChatMessagePromptTemplate,
-    SemanticSimilarityExampleSelector,
+from ktem.reasoning.prompt_optimization import (
+    FewshotRewriteQuestionPipeline,
+    RewriteQuestionPipeline,
 )
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from ktem.utils.render import Render
 from theflow.settings import settings as flowsettings
 
 from kotaemon.base import (
@@ -189,15 +185,6 @@ DEFAULT_QA_FIGURE_PROMPT = (
     "{context}\n"
     "Question: {question}\n"
     "Answer: "
-)
-
-DEFAULT_REWRITE_PROMPT = (
-    "Given the following question, rephrase and expand it "
-    "to help you do better answering. Maintain all information "
-    "in the original question. Keep the question as concise as possible. "
-    "Give answer in {lang}\n"
-    "Original question: {question}\n"
-    "Rephrased question: "
 )
 
 
@@ -427,128 +414,6 @@ class AnswerWithContextPipeline(BaseComponent):
         matches = re.findall(image_pattern, evidence)
         context = re.sub(image_pattern, "", evidence)
         return context, matches
-
-
-class RewriteQuestionPipeline(BaseComponent):
-    """Rewrite user question
-
-    Args:
-        llm: the language model to rewrite question
-        rewrite_template: the prompt template for llm to paraphrase a text input
-        lang: the language of the answer. Currently support English and Japanese
-    """
-
-    llm: ChatLLM = Node(default_callback=lambda _: llms.get_default())
-    rewrite_template: str = DEFAULT_REWRITE_PROMPT
-
-    lang: str = "English"
-
-    def run(self, question: str) -> Document:  # type: ignore
-        prompt_template = PromptTemplate(self.rewrite_template)
-        prompt = prompt_template.populate(question=question, lang=self.lang)
-        messages = [
-            SystemMessage(content="You are a helpful assistant"),
-            HumanMessage(content=prompt),
-        ]
-        return self.llm(messages)
-
-
-class FewshotRewriteQuestionPipeline(RewriteQuestionPipeline):
-    """Rewrite user question
-
-    Args:
-        llm: the language model to rewrite question
-        rewrite_template: the prompt template for llm to paraphrase a text input
-        lang: the language of the answer. Currently support English and Japanese
-    """
-
-    llm: ChatLLM = Node(default_callback=lambda _: llms.get_default())
-    rewrite_template: str = DEFAULT_REWRITE_PROMPT
-    lang: str = "English"
-    final_prompt: ChatPromptTemplate
-
-    def create_example_selector(
-        self, examples, k: int = getattr(flowsettings, "N_PROMPT_OPT_EXAMPLES", 0)
-    ):
-        if not examples:
-            return None
-
-        embeddings = OpenAIEmbeddings(
-            openai_api_key=getattr(flowsettings, "KH_EMBEDDINGS")["openai"]["spec"][
-                "api_key"
-            ],
-            openai_api_type="openai",
-            openai_api_base=getattr(flowsettings, "KH_EMBEDDINGS")["openai"]["spec"][
-                "base_url"
-            ],
-            openai_api_version=getattr(flowsettings, "KH_EMBEDDINGS")["openai"]["spec"][
-                "api_version"
-            ],
-        )
-        vectorstore = Chroma(
-            "rephrase_question",
-            embeddings,
-            persist_directory=str(getattr(flowsettings, "KH_APP_DATA_DIR")),
-        )
-        example_selector = SemanticSimilarityExampleSelector(
-            vectorstore=vectorstore,
-            k=k,
-        )
-
-        return example_selector
-
-    def create_prompt(self, examples, example_selector):
-        example_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("human", self.rewrite_template.replace("{lang}", self.lang)),
-                ("ai", "{rewritten_question}"),
-            ]
-        )
-
-        if example_selector is not None:
-            few_shot_prompt = FewShotChatMessagePromptTemplate(
-                example_prompt=example_prompt,
-                input_variables=["question"],
-                example_selector=example_selector,
-            )
-        elif examples:
-            few_shot_prompt = FewShotChatMessagePromptTemplate(
-                example_prompt=example_prompt,
-                examples=examples,
-            )
-
-        final_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", "You are a helpful assistant"),
-                few_shot_prompt,
-                ("human", self.rewrite_template.replace("{lang}", self.lang)),
-            ]
-        )
-
-        return final_prompt
-
-    @classmethod
-    def get_pipeline(cls, examples):
-        pipeline = cls()
-        example_selector = pipeline.create_example_selector(examples)
-        prompt = pipeline.create_prompt(examples, example_selector)
-        pipeline.final_prompt = prompt
-
-        return pipeline
-
-    def run(self, question: str) -> Document:  # type: ignore
-        messages = self.final_prompt.format_messages(question=question)
-        new_messages = []
-        for message in messages:
-            if isinstance(message, langchain_core.messages.human.HumanMessage):
-                new_messages.append(HumanMessage(content=message.content))
-            elif isinstance(message, langchain_core.messages.system.SystemMessage):
-                new_messages.append(SystemMessage(content=message.content))
-            elif isinstance(message, langchain_core.messages.ai.AIMessage):
-                new_messages.append(AIMessage(content=message.content))
-
-        result = self.llm(new_messages)
-        return result
 
 
 class AddQueryContextPipeline(BaseComponent):
