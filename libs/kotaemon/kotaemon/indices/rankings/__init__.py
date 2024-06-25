6a9bf657895f2c3f9b7dd839e42ee652090ba5a4
doviettung96 <tunga9td@gmail.com>
2024-06-25 15:23:59 +0700

    Add LLMTrulens reranking model

--- libs/kotaemon/kotaemon/indices/rankings/__init__.py
+++ libs/kotaemon/kotaemon/indices/rankings/__init__.py
@@ -2,5 +2,12 @@ from .base import BaseReranking
 from .cohere import CohereReranking
 from .llm import LLMReranking
 from .llm_scoring import LLMScoring
+from .llm_trulens import LLMTrulensScoring
 
-__all__ = ["CohereReranking", "LLMReranking", "LLMScoring", "BaseReranking"]
+__all__ = [
+    "CohereReranking",
+    "LLMReranking",
+    "LLMScoring",
+    "BaseReranking",
+    "LLMTrulensScoring",
+]
