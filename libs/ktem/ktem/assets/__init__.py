2e92bd22785945799f8ceca501fac54e9c82ee89
Tadashi <tadashi@cinnamon.is>
2024-08-18 11:13:02 +0700

    fix: add env var for PDFJS prebuilt

--- libs/ktem/ktem/assets/__init__.py
+++ libs/ktem/ktem/assets/__init__.py
@@ -1,3 +1,5 @@
+import os
 from pathlib import Path
 
-PDFJS_PREBUILT_DIR: Path = Path(__file__).parent / "prebuilt" / "pdfjs-4.0.379-dist/"
+PDFJS_VERSION_DIST: str = os.getenv("PDFJS_VERSION_DIST", "pdfjs-4.0.379-dist")
+PDFJS_PREBUILT_DIR: Path = Path(__file__).parent / "prebuilt" / PDFJS_VERSION_DIST
