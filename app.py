f44b37333468eb59d67d731c08c514f3432cc41a
trducng <trungduc1992@gmail.com>
2024-08-24 17:23:16 +0000

    fix: update default setting

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import os
 
-from ktem.main import App
 from theflow.settings import settings as flowsettings
 
 KH_APP_DATA_DIR = getattr(flowsettings, "KH_MARKDOWN_OUTPUT_DIR", ".")
-GRADIO_TEMP_DIR = os.getenv(
-    "GRADIO_TEMP_DIR", os.path.join(KH_APP_DATA_DIR, "gradio_tmp")
-)
+GRADIO_TEMP_DIR = os.getenv("GRADIO_TEMP_DIR", None)
+# override GRADIO_TEMP_DIR if it's not set
+if GRADIO_TEMP_DIR is None:
+    GRADIO_TEMP_DIR = os.path.join(KH_APP_DATA_DIR, "gradio_tmp")
+    os.environ["GRADIO_TEMP_DIR"] = GRADIO_TEMP_DIR
 
-app = App()
-demo = app.make()
-demo.queue().launch(
-    favicon_path=app._favicon,
-    inbrowser=True,
-    allowed_paths=[
-        "libs/ktem/ktem/assets",
-        GRADIO_TEMP_DIR,
-    ],
-)
+
+if __name__ == "__main__":
+    from ktem.main import App
+
+    app = App()
+    demo = app.make()
+    demo.queue().launch(
+        favicon_path=app._favicon,
+        inbrowser=True,
+        allowed_paths=[
+            "libs/ktem/ktem/assets",
+            GRADIO_TEMP_DIR,
+        ],
+    )
