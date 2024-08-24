52c90ed0ba17a469cda37bacdd1ba012e7d9670f
taprosoft <tadashi@cinnamon.is>
2024-08-24 08:52:27 +0000

    fix: update launch command

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import os
+
 from ktem.main import App
+
+GRADIO_TEMP_DIR = os.getenv("GRADIO_TEMP_DIR", "./gradio_tmp")
 
 app = App()
 demo = app.make()
-demo.queue().launch(favicon_path=app._favicon, inbrowser=True)
+demo.queue().launch(
+    favicon_path=app._favicon,
+    inbrowser=True,
+    allowed_paths=[
+        "libs/ktem/ktem/assets",
+        GRADIO_TEMP_DIR,
+    ],
+)
