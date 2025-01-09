f9af6993825ad051eb38a34311427ae93a1b74e1
Varun Sharma <contactvarun27@gmail.com>
2025-01-09 22:52:13 +0000

    fix: base path

--- libs/ktem/ktem/utils/render.py
+++ libs/ktem/ktem/utils/render.py
@@ -1,9 +1,12 @@
+import os
 import os.path
 
 import markdown
 from fast_langdetect import detect
 
 from kotaemon.base import RetrievedDocument
+
+BASE_PATH = os.environ.get("GRADIO_ROOT_PATH", "")
 
 
 def is_close(val1, val2, tolerance=1e-9):
@@ -98,7 +101,7 @@ class Render:
 
         return f"""
         {html_content}
-        <a href="#" class="pdf-link" data-src="/file={pdf_path}" data-page="{page_idx}" data-search="{highlight_text}" data-phrase="{phrase}">
+        <a href="#" class="pdf-link" data-src="{BASE_PATH}/file={pdf_path}" data-page="{page_idx}" data-search="{highlight_text}" data-phrase="{phrase}">
             [Preview]
         </a>
         """  # noqa
