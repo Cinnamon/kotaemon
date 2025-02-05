e90f89577ea5224e98ca9f9b1b5e4afe8fa657e3
Tadashi <tadashi@cinnamon.is>
2025-02-05 10:37:12 +0700

    fix: lanceDB query with empty file_ids

--- libs/kotaemon/kotaemon/storages/docstores/lancedb.py
+++ libs/kotaemon/kotaemon/storages/docstores/lancedb.py
@@ -98,6 +98,9 @@ class LanceDBDocumentStore(BaseDocumentStore):
         if not isinstance(ids, list):
             ids = [ids]
 
+        if len(ids) == 0:
+            return []
+
         id_filter = ", ".join([f"'{_id}'" for _id in ids])
         try:
             document_collection = self.db_connection.open_table(self.collection_name)
